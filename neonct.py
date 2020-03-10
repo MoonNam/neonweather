10dc8771c6ec07fd94ee0c2109b6276c24faa3c9
MoonNam <61456345+MoonNam@users.noreply.github.com>
2020-03-11 06:26:38 +0900

    Update neonct.py

--- neonct.py
+++ neonct.py
@@ -18,7 +18,12 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith("/퇴근"):
-        await message.channel.send("오늘은 야근하는 날 !")
+        now_time1 =(str(now.month) + "월 " + str(now.day) + "일 | " + str(now.hour) + "시" + str(now.minute) + "분")
+    embed = discord.Embed(color=0xff0000)
+    embed.add_field(name="네온 출석부", value=message.author.display_name + " 님이 퇴근 하셨습니다.　　" + '\n\n' + now_time1, inline=True)
+    embed.set_thumbnail(url=message.author.avatar_url)
+    await message.channel.send(embed=embed)
+
 
     if message.content.startswith("/출근"):
         now_time =(str(now.month) + "월 " + str(now.day) + "일 | " + str(now.hour) + "시" + str(now.minute) + "분")
