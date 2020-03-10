7c967b7e01d4a75d1a5595709a55cc7dc1e54563
MoonNam <61456345+MoonNam@users.noreply.github.com>
2020-03-11 07:24:28 +0900

    Update neonct.py

--- neonct.py
+++ neonct.py
@@ -3,7 +3,6 @@ import datetime
 import os
 
 client = discord.Client()
-now = datetime.datetime.now()
 
 @client.event
 async def on_ready():
@@ -29,12 +28,11 @@ async def on_message(message):
             await message.channel.send("권한이 없습니다.")
 
     if message.content.startswith("/퇴근"):
-        await message.channel.send(message.author.display_name +"님이 퇴근하셨습니다." + '\n\n' + (str(now.month) + "월 " + str(now.day) + "일 | " + str(now.hour) + "시" + str(now.minute) + "분"))
+        await message.channel.send(message.author.display_name +" 님이 퇴근하셨습니다.")
 
     if message.content.startswith("/출근"):
-        now_time =(str(now.month) + "월 " + str(now.day) + "일 | " + str(now.hour) + "시" + str(now.minute) + "분")
     embed = discord.Embed(color=0xff0000)
-    embed.add_field(name="네온 출석부", value=message.author.display_name + " 님이 출근하셨습니다.　　" + '\n\n' + now_time, inline=True)
+    embed.add_field(name="네온 출석부", value=message.author.display_name + " 님이 출근하셨습니다.)
     embed.set_thumbnail(url=message.author.avatar_url)
     await message.channel.send(embed=embed)
 
