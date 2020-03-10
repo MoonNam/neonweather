e25dc501cd5291a6dc06ef68688a5b4de814d9f6
MoonNam <61456345+MoonNam@users.noreply.github.com>
2020-03-11 06:50:49 +0900

    Update neonct.py

--- neonct.py
+++ neonct.py
@@ -11,7 +11,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print("------------------")
-    game = discord.Game("출근")
+    game = discord.Game("야근")
     await client.change_presence(status=discord.Status.online, activity=game)
 
 
@@ -21,16 +21,12 @@ async def on_message(message):
         now_time1 =(str(now.month) + "월 " + str(now.day) + "일 | " + str(now.hour) + "시" + str(now.minute) + "분")
     embed = discord.Embed(color=0xff0000)
     embed.add_field(name="네온 출석부", value=message.author.display_name + " 님이 퇴근 하셨습니다.　　" + '\n\n' + now_time1, inline=True)
-    embed.set_thumbnail(url=message.author.avatar_url)
     await message.channel.send(embed=embed)
 
-@client.event
-async def on_message(message):
     if message.content.startswith("/출근"):
         now_time =(str(now.month) + "월 " + str(now.day) + "일 | " + str(now.hour) + "시" + str(now.minute) + "분")
     embed = discord.Embed(color=0xff0000)
     embed.add_field(name="네온 출석부", value=message.author.display_name + " 님이 출근 하셨습니다.　　" + '\n\n' + now_time, inline=True)
-    embed.set_thumbnail(url=message.author.avatar_url)
     await message.channel.send(embed=embed)
 
 access_token = os.environ["BOT_TOKEN"]
